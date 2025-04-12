38acd8438e99d6681e38f06235e87138fec62091
onkyou39 <61075134+onkyou39@users.noreply.github.com>
2025-04-12 23:42:15 +0300

    initial commit

---
+++ include/config.example.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Wi-Fi настройки
+#define WIFI_SSID     "your-wifi-ssid"
+#define WIFI_PASSWORD "your-wifi-password"
+
+// API ключи
+#define API_KEY       "your-api-key"
+
+#endif
